7491a52f9a733c31ba3240a48fd4549e850a7c48
ohmyoungjin <sisaudwls@naver.com>
2022-12-08 18:09:50 +0900

    20221208 :: itemservice-db JdbcTemplate (1)

--- src/main/java/hello/itemservice/repository/ItemSearchCond.java
+++ src/main/java/hello/itemservice/repository/ItemSearchCond.java
@@ -2,6 +2,10 @@ package hello.itemservice.repository;
 
 import lombok.Data;
 
+//Repository 에서 사용되는 객체 이기때문에
+//package 흐름상 Repository 밑에 있다.
+//Service 밑에 있으면 의존 관계 흐름이 깨지게 된다
+//Repository 에서 필요한 부분인데 Service 단에 있으면 Repository 는 Service package를 의존하게 된다.
 @Data
 public class ItemSearchCond {
 
